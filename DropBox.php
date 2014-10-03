56bb888b91f436c365c79f5ca79e677de3028b5e
Alexandre Girard <git@alexgirard.com>
2014-10-03 16:51:43 +0200

    clean filename during convert

--- DropBox.php
+++ DropBox.php
@@ -63,8 +63,8 @@ class DropBox {
       }
 
       $data = array();
-      $data['title'] = htmlspecialchars (trim ( basename($file, '.' + $extension) ));
-      $data['filename'] = basename($file, '.' + $extension);
+      $data['title'] = htmlspecialchars (trim ( pathinfo($file, PATHINFO_FILENAME) ));
+      $data['filename'] = pathinfo($file, PATHINFO_FILENAME);
       $data['gated'] = '0';
       $data['private'] = '0';
       $data['disable_embed'] = '0';
